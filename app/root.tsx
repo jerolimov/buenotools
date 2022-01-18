f98c06a94fbedde5eafdf89c42d2eeef1402e86f
Christoph Jerolimov <jerolimov+git+2022@gmail.com>
2022-01-18 22:56:46 +0100

    Add 404 page

--- app/root.tsx
+++ app/root.tsx
@@ -6,7 +6,8 @@ import {
   MetaFunction,
   Outlet,
   Scripts,
-  ScrollRestoration
+  ScrollRestoration,
+  useCatch
 } from "remix";
 import styles from "./tailwind.css";
 
@@ -32,6 +33,56 @@ export default function App() {
         <ScrollRestoration />
         <Scripts />
         {process.env.NODE_ENV === "development" && <LiveReload />}
+      </body>
+    </html>
+  );
+}
+export function CatchBoundary() {
+  const caught = useCatch();
+  return (
+    <html lang="en">
+      <head>
+        <title>Oops!</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <div className="bg-white min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+          <div className="max-w-max mx-auto">
+            <main className="sm:flex">
+              <p className="text-4xl font-extrabold text-indigo-600 sm:text-5xl">
+                {caught.status}
+              </p>
+              <div className="sm:ml-6">
+                <div className="sm:border-l sm:border-gray-200 sm:pl-6">
+                  <h1 className="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
+                    {caught.statusText}
+                  </h1>
+                  <p className="mt-1 text-base text-gray-500">
+                    Please check the URL in the address bar and try again.
+                  </p>
+                </div>
+                <div className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
+                  <a
+                    href="#"
+                    className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    Go back home
+                  </a>
+                  <a
+                    href="#"
+                    className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    Contact support
+                  </a>
+                </div>
+              </div>
+            </main>
+          </div>
+        </div>
+        <Scripts />
       </body>
     </html>
   );
